5ce84a9ea3793d9cd8455b559b12a781aa98976c
Pablo Santana <pablosntojd@gmail.com>
2024-07-02 20:01:11 +0100

    fix: notfound new text

--- front/src/pages/NotFound/NotFound.jsx
+++ front/src/pages/NotFound/NotFound.jsx
@@ -5,6 +5,7 @@ const NotFound = () => {
     <>
       <h1>Ooops... ERROR 404</h1>
       <h4>The page you're trying to access doesn't exist</h4>
+      <h4>Try again refreshing</h4>
     </>
   )
 }
