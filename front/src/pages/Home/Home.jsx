bff6eb675a4922e8e8ebadcac7ef0eb7b982c033
Pablo Santana <pablosntojd@gmail.com>
2024-07-02 23:40:43 +0100

    feat: main structure for title page

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -2,7 +2,19 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div id='title-page'>
+      <div id='title-left-container'>
+        <div id='title-container'>
+          <h1 className='title'>PREPÁRATE PARA TU</h1>
+          <h1 className='title' id='yellow-title'>PRÓXIMO ENTRENO</h1>
+        </div>
+        <button id='train-btn'>
+          <a href="">¡PRUEBA NUESTRO ENTRENAMIENTO!</a>
+        </button>
+      </div>
+      <div id='video-container'>
+        <video src=""></video>
+      </div>
     </div>
   )
 }
