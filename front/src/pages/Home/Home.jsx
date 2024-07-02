0f844dde31e83ec56761f08ed23d186f2761f481
Pablo Santana <pablosntojd@gmail.com>
2024-07-03 00:21:11 +0100

    feat: main title mobile version with button

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -5,9 +5,11 @@ const Home = () => {
     <div id='title-page'>
       <div id='title-left-container'>
         <div id='title-container'>
-          <h1 className='title'>PREPÁRATE PARA TU</h1>
-          <h1 className='title' id='yellow-title'>PRÓXIMO ENTRENO</h1>
+          <h1 className='title'>PREPÁRATE PARA</h1>
+          <h1 className='title'>TU <span id='yellow-title'>PRÓXIMO</span></h1>
+          <h1 className='title' id='yellow-title'>ENTRENAMIENTO</h1>
         </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita quasi.</p>
         <button id='train-btn'>
           <a href="">¡PRUEBA NUESTRO ENTRENAMIENTO!</a>
         </button>
