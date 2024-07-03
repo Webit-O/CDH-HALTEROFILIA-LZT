2f5b8f8aa708e310c75d0560f5c7daeabe781c03
Pablo Santana <pablosntojd@gmail.com>
2024-07-03 01:50:58 +0100

    fix: change id to classname yellow-title

--- front/src/components/Title-page/Title-page.jsx
+++ front/src/components/Title-page/Title-page.jsx
@@ -6,8 +6,8 @@ const TitlePage = () => {
       <div id='title-left-container'>
         <div id='title-container'>
           <h1 className='title'>PREPÁRATE PARA</h1>
-          <h1 className='title'>TU <span id='yellow-title'>PRÓXIMO</span></h1>
-          <h1 className='title' id='yellow-title'>ENTRENAMIENTO</h1>
+          <h1 className='title'>TU <span className='yellow-title'>PRÓXIMO</span></h1>
+          <h1 className='title yellow-title'>ENTRENAMIENTO</h1>
         </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita quasi.</p>
         <button id='train-btn'>
--- front/src/components/Trainers/Trainers.jsx
+++ front/src/components/Trainers/Trainers.jsx
@@ -2,8 +2,9 @@ import './Trainers.css'
 
 const Trainers = () => {
   return (
-    <div>
-      
+    <div id='trainers-page'>
+      <h1>¡TRABAJA CON PROFESIONALES!</h1>
+      <h1 className='yellow-title'>CONSIGUE RESULTADOS REALES</h1>
     </div>
   )
 }
