9431f412a109651a687612c227c03e02a61c058a
Monica-R <moniroka_339@hotmail.com>
2024-07-09 19:27:40 +0100

    fix: changes in footer design

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
+import { FaInstagram, FaYoutube, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
 import NavbarFooter from '../NavbarFooter/NavbarFooter'
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <div id='footer'>
+    <div id='footer' className='footer'>
       <NavbarFooter />
+      <div className='footer__div'>
+        <section className='contact-section'>
+            <h3 className='contact-section__title'>CONTACTA</h3>
+            <p className='contact-section__p'>
+              <a href="#"><FaEnvelope className='icon'/><span>cdhalmas@gmail.com</span></a>
+              <a href="#" className='contact-map'><FaMapMarkerAlt className='icon'/><span>C/. Lanzarote, 35559, Lanzarote</span></a>
+            </p>
+        </section>
+        <section className='social-media'>
+            <h3 className='social-media__title'>SÍGUENOS</h3>
+            <p className='contact-section__p'>
+              <a href="#"><FaInstagram /></a>
+              <a href="#"><FaYoutube /></a>
+            </p>
+        </section>
+      </div>        
     </div>
   )
 }
--- front/src/components/NavbarFooter/NavbarFooter.jsx
+++ front/src/components/NavbarFooter/NavbarFooter.jsx
@@ -1,33 +1,45 @@
 import React from 'react'
-import { FaInstagram, FaYoutube, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'
 import './NavbarFooter.css'
 
+let navbars = [
+  {
+    classname: 'navbar-home',
+    links: [
+      { href: '#', text: 'INICIO' },
+      { href: '#', text: 'SOBRE NOSOTROS' },
+      { href: '#', text: 'TIENDA' },
+    ],
+  },
+  {
+    classname: 'navbar-legit',
+    links: [
+      { href: '#', text: 'POLÍTICA DE PRIVACIDAD' },
+      { href: '#', text: 'AVISO LEGAL' },
+      { href: '#', text: 'POLÍTICA DE COOKIES' },
+      { href: '#', text: 'INFORMACIÓN DE ENVÍO' },
+    ],
+  },
+]
 
 function NavbarFooter() {
+
+  const getRenderLinks = navbars.map((navbar, index) => {
+    return (
+        <>
+            <ul className={navbar.classname} key={index}>
+                { navbar.links.map((link, index) => {
+                    return (
+                        <li key={index}><a href={link.href}>{link.text}</a></li>
+                    )
+                })}
+            </ul>
+        </>
+    )
+  });
   return (
-    <div className='navbar-footer'>
-        <ul className="navbar-home">
-            <li><a href="#">INICIO</a></li>
-            <li><a href="#">SOBRE NOSOTROS</a></li>
-            <li><a href="#">TIENDA</a></li>
-        </ul>
-        <ul className='navbar-legit'>
-            <li><a href="#">POLÍTICA DE PRIVACIDAD</a></li>
-            <li><a href="#">AVISO LEGAL</a></li>
-            <li><a href="#">POLÍTICA DE COOKIES</a></li>
-            <li><a href="#">INFORMACIÓN DE ENVÍO</a></li>
-        </ul>
-        <ul className='navbar-contact'>
-            <h3 className='navbar-contact__title'>CONTACTA</h3>
-            <li><a href="#"><FaEnvelope /><span>cdhalmas@gmail.com</span></a></li>
-            <li className='contact-map'><a href="#"><FaMapMarkerAlt /><span>C/. Lanzarote, 35559, Lanzarote</span></a></li>
-        </ul>
-        <ul className='navbar-social'>
-            <h3 className='navbar-social__title'>SÍGUENOS</h3>
-            <li><a href="#"><FaInstagram /></a></li>
-            <li><a href="#"><FaYoutube /></a></li>
-        </ul>
-    </div>
+    <footer className='navbar-footer'>
+        { getRenderLinks }
+    </footer>
   )
 }
 
