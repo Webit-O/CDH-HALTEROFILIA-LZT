aa08d2e32f158ace36591fd55ace93ece3fb5aeb
Pablo Santana <pablosntojd@gmail.com>
2024-07-11 01:51:28 +0100

    feat: responsive design for ipad (min width 768px)

--- front/src/components/Trainers/Trainers.jsx
+++ front/src/components/Trainers/Trainers.jsx
@@ -9,16 +9,23 @@ const Trainers = () => {
           <img className='trainer-img' src="/img/cdh-trainer2.jpg" alt="Entrenadora de CDHalmas Andrea" />
           <p className='trainer-name'>ANDREA M.L.</p>
         </div>
+        <div id='center-wrapper'>
+          <div id='outlined-web' className='outlined-text-container'>
+            <h1 className='normal-text'>CONSIGUE RESULTADOS REALES</h1>
+            <h1 className='text-shadow'>CONSIGUE RESULTADOS REALES</h1>
+          </div>
+          <p id='info-text-web'>Planeas presentarte a exámenes de oposiciones o necesitas encontrar tu propio camino en el mundo de la halterofilia, nosotros somos el equipo de profesionales que necesitas para cumplir tus objetivos.</p>
+        </div>
         <div className='img-wrapper'>
           <img className='trainer-img' src="/img/cdh-trainer1.jpg" alt="Entrenador de CDHalmas Jose" />
           <p className='trainer-name'>JOSE A.C.</p>
         </div>
       </div>
-      <div id='outlined-text-container'>
+      <div id='outlined-mobile' className='outlined-text-container'>
         <h1 className='normal-text'>CONSIGUE RESULTADOS REALES</h1>
         <h1 className='text-shadow'>CONSIGUE RESULTADOS REALES</h1>
       </div>
-      <p>Planeas presentarte a exámenes de oposiciones o necesitas encontrar tu propio camino en el mundo de la halterofilia, nosotros somos el equipo de profesionales que necesitas para cumplir tus objetivos.</p>
+      <p id='info-text-mobile'>Planeas presentarte a exámenes de oposiciones o necesitas encontrar tu propio camino en el mundo de la halterofilia, nosotros somos el equipo de profesionales que necesitas para cumplir tus objetivos.</p>
       <button id='info-btn'><a href="">MÁS INFO</a></button>
     </div>
   )
