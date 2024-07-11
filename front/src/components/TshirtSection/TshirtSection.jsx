687d0df02b4d03c961577aa9ee4787380f6d0cd7
Monica-R <moniroka_339@hotmail.com>
2024-07-11 12:42:33 +0100

    shop section added

--- front/src/components/TshirtSection/TshirtSection.jsx
+++ front/src/components/TshirtSection/TshirtSection.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import './TshirtSection.css'
+import Slider from 'react-slick'
 function TshirtSection() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 800,
+    cssEase: 'linear',
+    arrows: false
+  }
   return (
-    <>
-      
-    </>
+    <section className='products-section'>
+      <article className='products-info'>
+        <h2 className='products__h2'>¡NUESTROS PRODUCTOS <span>HALTEROFRIKIS!</span></h2>
+        <p className='products__p'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </article>
+      <article className='products-picture'>
+        <Slider {...settings}>
+          <div className="product-slider__item">
+            <img src="https://via.placeholder.com/300" alt="tshirt" />
+          </div>
+          <div className="product-slider__item">
+            <img src="https://via.placeholder.com/300" alt="tshirt" />
+          </div>
+          <div className="product-slider__item">
+            <img src="https://via.placeholder.com/300" alt="tshirt" />
+          </div>
+        </Slider>
+        <button className="product__button">PÁSATE POR NUESTRA TIENDA</button>     
+      </article>
+    </section>
   )
 }
 
--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import QuotesList from '../../components/QuotesList/QuotesList'
 import TrainSection from '../../components/TrainSection/TrainSection'
+import TshirtSection from '../../components/TshirtSection/TshirtSection'
 import './Home.css'
 
 const Home = () => {
   return (
     <>
+      <TshirtSection />
       <TrainSection />
       <QuotesList />
     </>
