f9e0c85a10837277b211bf9a2d4eeef4aed27bf1
Pablo Santana <pablosntojd@gmail.com>
2024-07-13 00:12:35 +0100

    feat: new about page and router link created

--- front/src/router/index.jsx
+++ front/src/router/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
 import Layout from "../layouts/Layout";
+import About from "../pages/About/About"
 
 const router = createBrowserRouter([
     {
@@ -12,7 +13,11 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home />
-            }
+            },
+            {
+              path: '/about',
+              element: <About />
+          }
         ]
     }
 ]);
