f66374117a8ab79e07c8d5d90a42eb4cdc877ae2
Pablo Santana <pablosntojd@gmail.com>
2024-07-13 00:09:30 +0100

    fix: improved navbar links to show correctly when minimun size at web view

--- front/src/components/Navlinks/Navlinks.jsx
+++ front/src/components/Navlinks/Navlinks.jsx
@@ -4,9 +4,9 @@ const Navlinks = () => {
   return (
     <li id='nav-list'>
         <ul><a href="">INICIO</a></ul>
+        <ul><a href="">ENTRENAMIENTO</a></ul>
         <ul><a href="">TIENDA</a></ul>
         <ul><a href="">SOBRE NOSOTROS</a></ul>
-        <ul><a href="">ENTRENAMIENTO</a></ul>
     </li>
   )
 }
