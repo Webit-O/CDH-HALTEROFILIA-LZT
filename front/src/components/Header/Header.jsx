d7f4366df2f24d2fd2614b767760402b33ea3b08
Pablo Santana <pablosntojd@gmail.com>
2024-07-13 00:19:36 +0100

    feat: added Link to home logo and navlink

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -2,13 +2,16 @@ import './Header.css'
 import {MdOutlineMenu} from 'react-icons/md'
 import {MdClose} from 'react-icons/md';
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 import Navlinks from '../Navlinks/Navlinks';
 
 const Header = () => {
   const [click, setclick] = useState(false);
   const Hamburger = (
     <>
-      <img id='logo-header' src="/img/cdhalmas-logo.png" alt="halmas logo" />
+      <Link to="/">
+          <img id='logo-header' src="/img/cdhalmas-logo.png" alt="halmas logo" />
+        </Link>
       <MdOutlineMenu className="HamburgerMenu" size="70px" onClick={() => setclick(!click)} />
     </>
   )
@@ -17,7 +20,9 @@ const Header = () => {
   return (
     <div>
       <div className='navbar' id='nav-web-style'>
-        <img id='logo-header' src="/img/cdhalmas-logo.png" alt="halmas logo" />
+        <Link to="/">
+          <img id='logo-header' src="/img/cdhalmas-logo.png" alt="halmas logo" />
+        </Link>
         <Navlinks />
       </div>
       <div className='navbar' id='nav-mobile-style'>
--- front/src/components/Navlinks/Navlinks.jsx
+++ front/src/components/Navlinks/Navlinks.jsx
@@ -1,9 +1,10 @@
 import './Navlinks.css'
+import { Link } from 'react-router-dom'
 
 const Navlinks = () => {
   return (
     <li id='nav-list'>
-        <ul><a href="">INICIO</a></ul>
+        <ul><a href=""><Link to="/">INICIO</Link></a></ul>
         <ul><a href="">ENTRENAMIENTO</a></ul>
         <ul><a href="">TIENDA</a></ul>
         <ul><a href="">SOBRE NOSOTROS</a></ul>
