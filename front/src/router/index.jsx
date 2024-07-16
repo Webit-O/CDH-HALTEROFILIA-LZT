19f51ab6db487d72f8570dc6304dd51c5f3b94b1
Monica-R <moniroka_339@hotmail.com>
2024-07-16 21:47:37 +0100

    Retail view added, /retail route added

--- front/src/router/index.jsx
+++ front/src/router/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
 import Layout from "../layouts/Layout";
+import Retail from "../pages/Retail/Retail";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home />
+            },
+            {
+                path: '/retail',
+                element: <Retail />
             }
         ]
     }
