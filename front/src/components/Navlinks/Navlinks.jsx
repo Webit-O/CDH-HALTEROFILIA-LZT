aa156e3fdc7bc107ca77af54014fe2bf9ad8fc8e
Monica-R <moniroka_339@hotmail.com>
2024-07-17 15:46:27 +0100

    edit: links

--- front/src/components/Navlinks/Navlinks.jsx
+++ front/src/components/Navlinks/Navlinks.jsx
@@ -6,7 +6,7 @@ const Navlinks = () => {
     <ul id='nav-list'>
         <NavLink to="/" exact activeClassName="active"><li>INICIO</li></NavLink>
         <NavLink to="/training" activeClassName="active"><li>ENTRENAMIENTO</li></NavLink>
-        <NavLink to="/store" activeClassName="active"><li>TIENDA</li></NavLink>
+        <NavLink to="/retail" activeClassName="active"><li>TIENDA</li></NavLink>
         <NavLink to="/about" activeClassName="active"><li>SOBRE NOSOTROS</li></NavLink>
     </ul>
   )
