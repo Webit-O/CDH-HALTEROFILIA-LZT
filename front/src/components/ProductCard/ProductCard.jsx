717f8df1160c9e8f044dda08a2dfc841c3bd1095
Monica-R <moniroka_339@hotmail.com>
2024-07-17 15:44:48 +0100

    feat: shop added

--- front/src/components/ProductCard/ProductCard.jsx
+++ front/src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCartPlus, FaHeart } from 'react-icons/fa'
 
 function ProductCard({ name, image, description, price }) {
   return (
@@ -7,7 +8,15 @@ function ProductCard({ name, image, description, price }) {
       <figcaption className="item-card-info">
         <h4 className="item-card-title">{ name }</h4>
         <span className="item-card-description">{ description }</span>
-        <span className="item-card-price">{ price }</span>
+        <span className="item-card-price">{ price } €</span>
+        <div className="item-card-options">
+          <button className="item-card-button">
+            <FaCartPlus className="item-card-icon" title='Comprar'/>
+          </button>
+          <button className="item-card-button">
+            <FaHeart className="item-card-icon" title='Añadir a lista de deseos'/>
+          </button>
+        </div>
       </figcaption>
     </figure>
   )
--- front/src/pages/Retail/Retail.jsx
+++ front/src/pages/Retail/Retail.jsx
@@ -6,24 +6,38 @@ import './Retail.css'
 const products = [
   {
     id: 1,
-    image: 'https://via.placeholder.com/150',
+    image: '/img/blue-tshirt.webp',
     name: 'Product 1',
     description: 'Description 1',
-    price: 100,
+    price: 26.00,
   },
   {
     id: 2,
-    image: 'https://via.placeholder.com/150',
+    image: '/img/green-tshirt.webp',
     name: 'Product 2',
     description: 'Description 2',
-    price: 200,
+    price: 26.59,
   },
   {
     id: 3,
-    image: 'https://via.placeholder.com/150',
+    image: 'img/salmon-tshirt.webp',
     name: 'Product 3',
     description: 'Description 3',
-    price: 300,
+    price: 27.15,
+  },
+  {
+    id: 4,
+    image: 'img/tshirt-run.png',
+    name: 'Product 4',
+    description: 'Description 4',
+    price: 27.00,
+  },
+  {
+    id: 5,
+    image: 'img/t-shirt1.png',
+    name: 'Product 5',
+    description: 'Description 5',
+    price: 32.00,
   },
 ];
 
@@ -52,7 +66,16 @@ function Retail() {
 
   return (
     <main className='retail'>
-     {arrayProducts}
+      <div className="categories-list">
+        <ul>
+          <li>Category 1</li>
+          <li>Category 2</li>
+          <li>Category 3</li>
+        </ul>
+      </div>
+      <div className="products-list">
+        { arrayProducts }
+      </div>
     </main>
   )
 }
