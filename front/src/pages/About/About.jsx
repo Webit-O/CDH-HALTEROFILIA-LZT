dadd9c95754d31869330a64823e8116f70b1818c
Pablo Santana <pablosntojd@gmail.com>
2024-07-17 01:49:01 +0100

    feat: about us page

--- front/src/pages/About/About.jsx
+++ front/src/pages/About/About.jsx
@@ -2,8 +2,19 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-      
+    <div id='about-container'>
+      <div>
+        <h1 className='title'>CLUB DEPORTIVO HALMAS</h1>
+        <h1 className='yellow-title'>Nuestro objetivo, que seas más fuerte</h1>
+      </div>
+      <div>
+        <p><span>Somos especialistas en ejercicios de habilidades y destreza relacionados con el deporte: </span></p>
+        <p>entrenamiento articular, óseo y tendinoso para el rendimiento, la prevención y regeneración de lesiones.
+        Queremos hacer la carrera del deportista apta a largo plazo con el fin de tener más años para ver su arte independientemente de su nivel o edad.</p>
+      </div>
+      <div className='img-wrapper'>
+        <img src="/img/cdh-about.jpg" alt="trainers from cdhalmas Jose and Andrea" />
+      </div>
     </div>
   )
 }
