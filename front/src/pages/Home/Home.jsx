b2e7535c429089b2babf2999dc19f0d2d6801594
Pablo Santana <pablosntojd@gmail.com>
2024-07-17 23:03:10 +0100

    feat: animation on scroll from left and right at homepage

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -6,13 +6,29 @@ import TitlePage from '../../components/Title-page/Title-page'
 import Trainers from '../../components/Trainers/Trainers'
 
 const Home = () => {
+
+  window.addEventListener('scroll', reveal);
+  function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const revealTop = reveals[i].getBoundingClientRect().top;
+      const revealPoint = 150;
+      
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add('active');
+      } else {
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
   return (
     <div id='home-container'>
       <TitlePage/>
-      <Trainers/>
-      <TshirtSection />
-      <TrainSection />
-      <QuotesList />
+      <Trainers className='reveal right'/>
+      <TshirtSection className='reveal left'/>
+      <TrainSection className='reveal'/>
+      <QuotesList className='reveal'/>
     </div>
   )
 }
