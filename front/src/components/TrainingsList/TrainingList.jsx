1183ab763900a50aebd8d7a25221846529ac2644
Monica-R <moniroka_339@hotmail.com>
2024-07-22 19:40:11 +0100

    fix: route images in TrainingList.jsx

--- front/src/components/TrainingsList/TrainingList.jsx
+++ front/src/components/TrainingsList/TrainingList.jsx
@@ -3,15 +3,15 @@ import './TrainingList.css'
 const TrainingList = () => {
   //HAY QUE CORREGIR LAS IMAGENES PARA QUE TENGAN TODAS LAS MISMAS PROPORCIONES (AHORA MISMO SON SOLO DE PRUEBA)
     const trainingImages = [
-      'public/img/training-benchpress.png',
-      'public/img/training-deadlift.png',
-      'public/img/training-dumbbell.png',
-      'public/img/training-frontsquat.png',
-      'public/img/training-hipthrust.png',
-      'public/img/training-lunges.png',
-      'public/img/training-pressmilitar.png',
-      'public/img/training-pullups.png',
-      'public/img/training-remo.png'
+      'img/training-benchpress.png',
+      'img/training-deadlift.png',
+      'img/training-dumbbell.png',
+      'img/training-frontsquat.png',
+      'img/training-hipthrust.png',
+      'img/training-lunges.png',
+      'img/training-pressmilitar.png',
+      'img/training-pullups.png',
+      'img/training-remo.png'
     ]
   return (
     <div className='list-wrapper'>
