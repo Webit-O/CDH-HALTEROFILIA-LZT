6f5abe29841a034c90f67ad774a13d566aa02c9b
Monica-R <moniroka_339@hotmail.com>
2024-07-25 18:23:33 +0100

    minimal-changes

--- front/src/pages/About/About.jsx
+++ front/src/pages/About/About.jsx
@@ -8,8 +8,7 @@ const About = () => {
         <span className='about-subtitle'>Nuestro objetivo, que seas más fuerte</span>
       </div>
       <div>
-        <p>Somos especialistas en ejercicios de habilidades y destreza relacionados con el deporte: </p>
-        <p>entrenamiento articular, óseo y tendinoso para el rendimiento, la prevención y regeneración de lesiones.
+        <p>Somos especialistas en ejercicios de habilidades y destreza relacionados con el deporte: entrenamiento articular, óseo y tendinoso para el rendimiento, la prevención y regeneración de lesiones.
         Queremos hacer la carrera del deportista apta a largo plazo con el fin de tener más años para ver su arte independientemente de su nivel o edad.</p>
       </div>
       <div className='img-wrapper'>
